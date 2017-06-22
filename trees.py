3eb03a9c0c47c1397cac890312ed694745a7769b
chrism311 <rojascm02@gmail.com>
2017-06-21 23:00:12 -0500

    modified trees script

--- trees.py
+++ trees.py
@@ -5,12 +5,17 @@ data = np.genfromtxt('plrx.txt')
 
 X = data[:,:12]
 y = data[:, -1]
+scores = []							#array that will contain the accuracy scores for each classification run
 
-clf = ensemble.RandomForestClassifier(n_estimators=10)
-clf = clf.fit(X,y)
+def trees(X,y):							#random forest classifier defined as a function
+	clf = ensemble.RandomForestClassifier(n_estimators=10)
+	clf.fit(X,y)
+	results = clf.predict(X)
+	return  results;
 
-results = clf.predict(X)
+for x in range(100):						#'for' loop to run the clf for 100 iterations
+	accuracy = metrics.accuracy_score(y, trees(X,y))
+	scores.append(accuracy)
 
-accuracy = metrics.accuracy_score(y, results)
-print accuracy
+print 100*np.mean(scores)					#average of the 100 scores
 
