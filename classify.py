e52eb0475ec952705569d7b2d14bcc75c2cf25a9
chrism311 <rojascm02@gmail.com>
2017-06-24 19:14:58 -0500

    changes to classify and trees files

--- classify.py
+++ classify.py
@@ -11,40 +11,58 @@ clf2 = svm.SVC(gamma = 5)				#SVM classifier
 clf3 = ensemble.RandomForestClassifier(n_estimators=10)	#Random Forest classifier
 
 #empty arrays to hold classifier accuracy scores
-knn_scores = []
-svm_scores= []
-trees_scores = []
+knn_scores1 = []
+svm_scores1= []
+trees_scores1 = []
 
 #Spliting the data into training and testing data
-kf = model_selection.KFold(n_splits=5 )
+kf = model_selection.KFold(n_splits=2 )
 for train , test in kf.split(X):
 	clf1.fit(X[train],y[train])
 	clf2.fit(X[train],y[train])
 	clf3.fit(X[train],y[train])
-	acc_knn= clf1.score(X[test], y[test])
-	acc_svm= clf2.score(X[test],y[test])
-	acc_trees= clf3.score(X[test],y[test])
-	knn_scores.append(acc_knn)
-	svm_scores.append(acc_svm)
-	trees_scores.append(acc_trees)
+	acc_knn1= clf1.score(X[test],y[test])
+	acc_svm1= clf2.score(X[test],y[test])
+	acc_trees1= clf3.score(X[test],y[test])
+	knn_scores1.append(acc_knn1)
+	svm_scores1.append(acc_svm1)
+	trees_scores1.append(acc_trees1)
 
-print "k-NN score: ",100*np.mean(knn_scores)
-print"SVM score: ", 100*np.mean(svm_scores)
-print "Random Forest score: ", 100*np.mean(trees_scores)
+print "\nUsing k-fold:\n", "k-NN score: ",100*np.mean(knn_scores1)
+print"SVM score: ", 100*np.mean(svm_scores1)
+print "Random Forest score: ", 100*np.mean(trees_scores1)
 
-################ Ignore everything below this comment, work from previous script ################
-#fitting the data with the model
-result_knn = clf1.predict(X)	#prediction with training data
-result_svm = clf2.predict(X)	#prediction of training data
+knn_scores2 = []
+svm_scores2 = []
+trees_scores2 = []
 
-training_accuracy_knn = metrics.accuracy_score(y, result_knn)	
-training_accuracy_svm = metrics.accuracy_score(y, result_svm)	
-#training accuracy for nearest neighbors
-c_matrix_knn = metrics.confusion_matrix(y, result_knn)		
-c_matrix_svm = metrics.confusion_matrix(y, result_svm)		
-#confusion matrix
-c_matrix_svm = metrics.confusion_matrix(y, result_svm)	#confusion matrix
-#print "kNN training accuracy:",training_accuracy_knn
-#print "SVM training accuracy:",training_accuracy_svm
-#print "Confusion matrix for knn:\n", c_matrix_knn
-#print "Confusion matrix for svm:\n", c_matrix_svm
+loo = model_selection.LeaveOneOut()
+for train, test in loo.split(X):
+	clf1.fit(X[train],y[train])
+	clf2.fit(X[train],y[train])
+	clf3.fit(X[train],y[train])
+	acc_knn2 = clf1.score(X[test],y[test])
+	acc_svm2 = clf2.score(X[test],y[test])
+	acc_trees2 = clf3.score(X[test],y[test])
+	knn_scores2.append(acc_knn2)
+	svm_scores2.append(acc_svm2)
+	trees_scores2.append(acc_trees2)
+print "\nUsing Leave One Out:\n","k-nn score:", 100*np.mean(knn_scores2)
+print "SVM score:",100*np.mean(svm_scores2)
+print "Random forest score:",100*np.mean(trees_scores2)
+
+knn_scores3 = model_selection.cross_val_score(clf1, X, y, cv=2)
+svm_scores3 = model_selection.cross_val_score(clf2, X, y, cv=2)
+trees_scores3 = model_selection.cross_val_score(clf3, X, y, cv=2)
+print "\nUsing CV metrics:\n","k-NN score:", 100*np.mean(knn_scores3)
+print "SVM score:",100*np.mean(svm_scores3)
+print "Random Forest score:",100*np.mean(trees_scores3)
+
+
+
+
+
+
+
+#c_matrix_knn = metrics.confusion_matrix(y, result_knn)		
+#c_matrix_svm = metrics.confusion_matrix(y, result_svm)		
--- trees.py
+++ trees.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn import ensemble, metrics
+from sklearn import ensemble, metrics, model_selection
 
 data = np.genfromtxt('plrx.txt')
 
@@ -8,14 +8,23 @@ y = data[:, -1]
 scores = []							#array that will contain the accuracy scores for each classification run
 
 def trees(X,y):							#random forest classifier defined as a function
-	clf = ensemble.RandomForestClassifier(n_estimators=10)
+	clf = ensemble.RandomForestClassifier(n_estimators=10,)
 	clf.fit(X,y)
 	results = clf.predict(X)
-	return  results;
+	return results;
 
 for x in range(100):						#'for' loop to run the clf for 100 iterations
 	accuracy = metrics.accuracy_score(y, trees(X,y))
 	scores.append(accuracy)
 
-print 100*np.mean(scores)					#average of the 100 scores
 
+clf = ensemble.RandomForestClassifier(n_estimators=30,max_depth=3)
+loo_scores= []
+
+loo = model_selection.LeaveOneOut()
+for train, test in loo.split(X):
+	clf.fit(X[train], y[train])
+	acc = clf.score(X[test], y[test])
+	loo_scores.append(acc)
+
+print 100*np.mean(loo_scores)
